b1c4a7db0759f3034a87e8060f3e5d43cf2940a4
aceka <aceka@resqsoft.com>
2021-03-24 02:16:49 -0400

    added build

---
+++ build/precache-manifest.4f34573078328ea9e3f89fde26813cee.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844f707c10875eb9a3153bc4c4115753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654a31ba124a8847e1e3",
+    "url": "/static/css/2.27efe2fd.chunk.css"
+  },
+  {
+    "revision": "3de7ede10383ca8428b6",
+    "url": "/static/css/main.df5da53b.chunk.css"
+  },
+  {
+    "revision": "654a31ba124a8847e1e3",
+    "url": "/static/js/2.385b8f6b.chunk.js"
+  },
+  {
+    "revision": "447f479a1d2ff157ccaac090ec3c3b8e",
+    "url": "/static/js/2.385b8f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de7ede10383ca8428b6",
+    "url": "/static/js/main.e9b86b30.chunk.js"
+  },
+  {
+    "revision": "0edaeaddd1b3fd3ae8ee",
+    "url": "/static/js/runtime-main.009a96a8.js"
+  },
+  {
+    "revision": "67e2b9d795fc068c08cef7e07ea5171b",
+    "url": "/static/media/avatar-1.67e2b9d7.jpg"
+  },
+  {
+    "revision": "02aea0c2c148263b4c4de63a52284803",
+    "url": "/static/media/avatar-2.02aea0c2.jpg"
+  },
+  {
+    "revision": "41a180f34b2da436150ac0442c88b9f2",
+    "url": "/static/media/avatar-3.41a180f3.jpg"
+  },
+  {
+    "revision": "e0ab315a0e2e82eb9fec1387bae6600d",
+    "url": "/static/media/avatar-4.e0ab315a.jpg"
+  },
+  {
+    "revision": "b5c826fc619edc4cc35993d0cd3c827c",
+    "url": "/static/media/bg-ico-hero.b5c826fc.jpg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "/static/media/dripicons-v2.11fc83ae.woff"
+  },
+  {
+    "revision": "7e12564e72400735ae5b671780f6a9f7",
+    "url": "/static/media/dripicons-v2.7e12564e.eot"
+  },
+  {
+    "revision": "835984438066bd8944a7692e331152a7",
+    "url": "/static/media/dripicons-v2.83598443.svg"
+  },
+  {
+    "revision": "cf09c981aeaa8736810133ab1148e4a3",
+    "url": "/static/media/dripicons-v2.cf09c981.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "13652570b25022df253dd8e694cdcd5a",
+    "url": "/static/media/img-4.13652570.png"
+  },
+  {
+    "revision": "7821ebc744a5cd212ca59abdce32d27a",
+    "url": "/static/media/img-6.7821ebc7.png"
+  },
+  {
+    "revision": "5f29a91e4757ff169585789c4d180fd5",
+    "url": "/static/media/img-7.5f29a91e.png"
+  },
+  {
+    "revision": "a4629b6365cd7067c88dfee273ef5cb5",
+    "url": "/static/media/layout-1.a4629b63.jpg"
+  },
+  {
+    "revision": "5afd4b42bd0dcc7a6188c1e8c8a4b54a",
+    "url": "/static/media/layout-2.5afd4b42.jpg"
+  },
+  {
+    "revision": "06b3c2036bbdc7a43e88ecaedb073631",
+    "url": "/static/media/layout-3.06b3c203.jpg"
+  },
+  {
+    "revision": "202a69fb0f020289341d0a6fd6741d0f",
+    "url": "/static/media/logo-light.202a69fb.svg"
+  },
+  {
+    "revision": "4dbbacd2b2a1f0fd2f7939b1e1c3993b",
+    "url": "/static/media/logo.4dbbacd2.svg"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "/static/media/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "/static/media/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "/static/media/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "/static/media/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "913183e44a90bcc2bf414c35a1ae6d11",
+    "url": "/static/media/megamenu-img.913183e4.png"
+  },
+  {
+    "revision": "ba4e037e1b9676c5215541748eaa1e42",
+    "url": "/static/media/profile-img.ba4e037e.png"
+  },
+  {
+    "revision": "ce3344a2d07e5420a03671a70f79731a",
+    "url": "/static/media/yaward_logo.ce3344a2.svg"
+  }
+]);
